91168057a8a5b6cb987c878b582734b30886d045
RonKen-coder <ronalda21@hotmail.com>
2024-12-12 20:19:41 -0600

    Revert "Sever/small fixes"

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt';
 interface UserAttributes {
   id: number;
   username: string;
+  email: string;
   password: string;
 }
 
@@ -15,6 +16,7 @@ interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
 export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
   public id!: number;
   public username!: string;
+  public email!: string;
   public password!: string;
 
   public readonly createdAt!: Date;
@@ -37,6 +39,10 @@ export function UserFactory(sequelize: Sequelize): typeof User {
         primaryKey: true,
       },
       username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
         type: DataTypes.STRING,
         allowNull: false,
       },

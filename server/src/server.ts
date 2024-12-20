399d42a95ef89f9fd469be24149e8ff4b7a65fe5
Sinnema1 <142680381+Sinnema1@users.noreply.github.com>
2024-12-19 20:02:44 -0700

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ import { UserFactory } from './models/user.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const forceDatabaseRefresh = false;
+const forceDatabaseRefresh = true;
 
 
 // Initialize User model
